7b760855d01543113f15b054ba73e4d565ca0549
Joaquin Soto <joaquinsotorizzi@gmail.com>
2024-06-05 16:33:21 +0200

    show refactor

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,8 +1,20 @@
 class MealsController < ApplicationController
-  before_action :set_meal, only: [:destroy]
-  before_action :set_meal_plan, only: [:destroy]
+  before_action :set_meal, only: [:destroy, :update, :replace]
+  before_action :set_meal_plan, only: [:destroy, :update, :replace]
 
   def show
+  end
+
+  def replace
+    if params[:filtered_recipes_ids].nil?
+      @recipe = Recipe.where.not(id: @meal_plan.recipes.ids).sample
+    else
+      @recipe = Recipe.where(id: params[:filtered_recipes_ids]).where.not(id: @meal_plan.recipes.ids).sample
+    end
+
+    @meal.update(recipe: @recipe)
+    # raise
+    redirect_to meal_plan_path(@meal_plan), status: :see_other
   end
 
   def update
@@ -20,6 +32,10 @@ class MealsController < ApplicationController
   end
 
   def set_meal_plan
-    @meal_plan = MealPlan.find(Meal.find(params[:id]).meal_plan_id)
+    @meal_plan = @meal.meal_plan
+  end
+
+  def meal_params
+    params.require(:meal).permit(:nb_people, :recipe_id, :meal_plan_id)
   end
 end
