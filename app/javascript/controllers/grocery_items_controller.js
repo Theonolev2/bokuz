8ae8ac702e3cdcfbc3988aeac8bf881ee2d3fc40
Joaquin Soto <joaquinsotorizzi@gmail.com>
2024-06-06 17:03:01 +0200

    adding grocery item stimulus controller

--- app/javascript/controllers/grocery_items_controller.js
+++ app/javascript/controllers/grocery_items_controller.js
@@ -1,21 +1,32 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['bought']
-  connect() {
+  static targets = ['bought'];
+  static values = {
+    id: Number,
+  };
+  connect() {}
 
-    // mark_as_bought() {
+  markAsBought(event) {
 
-    //   if (this.boughtTarget.classList.contains("my-icon-bought")) {
-    //     this.boughtTarget.classList.remove("my-icon-bought")
-    //     this.boughtTarget.classList.add("my-icon-not-bought")
-    //   }
-    //   else {
-    //     this.boughtTarget.classList.remove("my-icon-not-bought")
-    //     this.boughtTarget.classList.add("my-icon-bought")
-    //   };
-    // }
-    console.log("grocery-items controller connected")
+    console.log(this.idValue);
+    const url = `../../grocery_items/${this.idValue}/mark_as_bought`;
+    console.log(url);
 
+    fetch(url, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'text/plain',
+        'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+      },
+      body: 'test',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // this.boughtTarget.classList.toggle("my-icon-not-bought");
+        // this.boughtTarget.classList.toggle("my-icon-bought");
+      }
+    )
   }
 }
