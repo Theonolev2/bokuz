60c7bb04648b89fbbc5ee8935b069c4d738df0b9
Theo Velon <theovelon@hotmail.fr>
2024-06-11 14:57:01 +0200

    feat: map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,36 +2,50 @@ import { Controller } from "@hotwired/stimulus";
 import mapboxgl from "mapbox-gl"; // Don't forget this!
 
 export default class extends Controller {
+  // create an array to store the markers created in the function addMarkersToMap
+
   static values = {
     apiKey: String,
     markers: Array,
-  };
+   };
 
   connect() {
+    this.markers = [];
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10",
+      });
+      this.#addUsersToMap();
+      this.#addMarkersToMap();
+      this.#fitMapToMarkers()
+  }
+
+  #addUsersToMap() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const popup = new mapboxgl.Popup().setHTML("<h1>You<h1>");
+      this.markers.push(new mapboxgl.Marker()
+        .setLngLat([position.coords.longitude, position.coords.latitude])
+        .setPopup(popup)
+        .addTo(this.map));
     });
-    this.#addMarkersToMap();
-    this.#fitMapToMarkers()
   }
 
   #addMarkersToMap() {
     this.markersValue.forEach(marker => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window_html);
-      new mapboxgl.Marker()
+      this.markers.push(new mapboxgl.Marker({ "color": "#b40219", "size": "small" })
         .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
-        .addTo(this.map)
+        .addTo(this.map));
     });
   }
 
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds();
-    this.markersValue.forEach(marker =>
-      bounds.extend([marker.lng, marker.lat])
+    this.markers.forEach(marker =>
+      bounds.extend([marker.getLngLat().lng, marker.getLngLat().lat])
     );
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   }
