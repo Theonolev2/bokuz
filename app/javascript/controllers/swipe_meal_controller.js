865d34cbed181ed5117c5d1a050666f171dc9622
Theo Velon <theovelon@hotmail.fr>
2024-06-13 14:23:27 +0200

    wip: swiper

--- app/javascript/controllers/swipe_meal_controller.js
+++ app/javascript/controllers/swipe_meal_controller.js
@@ -10,29 +10,60 @@ export default class extends Controller {
 let touchstartX = 0;
 let touchcurrentX = 0;
 let touchendX = 0;
+let baseWidth = 0;
 let isDragging = false;
 
-let gesturedZones = document.querySelectorAll('.card-meal');
+const gesturedZones = document.querySelectorAll('.card-meal');
+
+
+const maxWidth = gesturedZones[0].offsetWidth;
+const minWidth = Math. floor(maxWidth * 0.8); // 80% of the max width
+
 
 gesturedZones.forEach(function(gesturedZone) {
+  const iconZone = gesturedZone.nextElementSibling;
   gesturedZone.addEventListener('touchstart', function(event) {
     isDragging = true;
     touchstartX = event.changedTouches[0].screenX;
+    baseWidth = gesturedZone.offsetWidth;
   }, { passive: true });
 
   gesturedZone.addEventListener('touchmove', function(event) {
     if (!isDragging) return;
     touchcurrentX = event.changedTouches[0].screenX;
-    const translateX = Math.min(0, touchcurrentX - touchstartX);
-    console.log(gesturedZone.offsetWidth);
-    const cardWidth = gesturedZone.offsetWidth + translateX;
-    gesturedZone.setAttribute("style",`width:${cardWidth}px`);
+    const translateX = touchcurrentX - touchstartX;
+    // let cardWidth = 0;
+    // if (translateX < 0) {
+    //   cardWidth = Math.max(minWidth, (maxWidth + translateX));
+    // } else {
+    //   cardWidth = Math.min(maxWidth, (minWidth + translateX));
+    // }
+    console.log(translateX);
+    gesturedZone.style.transform = `translateX(${translateX}px)`;
+    iconZone.setAttribute("style",`width: ${Math.floor(Math.abs(translateX-10))}px`);
+    // gesturedZone.setAttribute("style",`transform: translateX(-${translateX}px)`);
   }, { passive: true });
 
   gesturedZone.addEventListener('touchend', function(event) {
     if (!isDragging) return;
     isDragging = false;
     touchendX = event.changedTouches[0].screenX;
+    gesturedZone.animate([
+      { transform: `translateX(-${150}px)` }
+    ], {
+      duration: 500,
+      iterations: 1,
+      fill: "forwards",
+      easing: "ease-in-out"
+    });
+    iconZone.animate([
+      { width: `${160}px` }
+    ], {
+      duration: 500,
+      iterations: 1,
+      fill: "forwards",
+      easing: "ease-in-out"
+    });
     handleGesure();
   }, false);
 });
@@ -40,9 +71,9 @@ gesturedZones.forEach(function(gesturedZone) {
 function handleGesure() {
     let swiped = 'swiped: ';
     if (touchendX < touchstartX) {
-        alert(swiped + 'left!');
+        // alert(swiped + 'left!');
     }
     if (touchendX > touchstartX) {
-        alert(swiped + 'right!');
+        // alert(swiped + 'right!');
     }
 }
