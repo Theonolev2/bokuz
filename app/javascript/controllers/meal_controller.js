a3c66302666db5a2a0ef53a3698384f5037c2af3
Joaquin Soto <joaquinsotorizzi@gmail.com>
2024-06-13 14:15:53 +0200

    changing styling of modal

--- app/javascript/controllers/meal_controller.js
+++ app/javascript/controllers/meal_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="meal"
 export default class extends Controller {
+  static targets = ["content"];
   connect() {}
 
   showDeleteModal(event) {
